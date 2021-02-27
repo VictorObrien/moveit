bc1546a198b8a60a33b82586d150888e7a2d200d
VictorObrien <victorobrien@hotmail.com>
2021-02-27 01:04:31 -0300

    Adding Context

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,24 +1,30 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContexts';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
   const hasActiveChallenge = true;
 
   return(
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}> 
-          <header>Ganhe 400 XP</header>
+          <header>Ganhe {activeChallenge.amount} XP</header>
 
           <main>
-            <img src="icons/body.svg"/>
+            <img src={`icons/${activeChallenge.type}.svg`}/>
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos.</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
             <button 
               type="button"
               className={styles.challengeFailedButton}
+              onClick={resetChallenge}
             >
               Falhei
             </button>
